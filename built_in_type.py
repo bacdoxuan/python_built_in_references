5ab4db565b76e7e276a2f8bdbdd21812eff8fb5a
Do Xuan Bac <bacdoxuan@gmail.com>
2023-05-15 14:55:41 +0700

    First commit

---
+++ built_in_type.py
@@ -0,0 +1,19 @@
+"""
+https://docs.python.org/3/library/stdtypes.html
+"""
+
+"""
+Some of built-in data types:
+-- Text Type: 	str
+-- Numeric Types: 	int, float, complex
+-- Sequence Types: 	list, tuple, range
+-- Mapping Type: 	dict
+-- Set Types: 	set, frozenset
+-- Boolean Type: 	bool
+-- Binary Types: 	bytes, bytearray, memoryview
+-- None Type: 	NoneType
+
+
+To get the type of data, use the built-in function:
+type()
+"""

3b147f336d1ddb79bcd923bbc4f6928ab59e6e63
Do Xuan Bac <bacdoxuan@gmail.com>
2025-05-07 09:16:10 +0700

    add new file

---
+++ built_in_keyword.py
@@ -0,0 +1,45 @@
+import keyword
+
+for kw in keyword.kwlist:
+    print(kw)
+
+"""
+Print result:
+
+False
+None  
+True  
+and   
+as    
+assert
+async
+await
+break
+class
+continue
+def
+del
+elif
+else
+except
+finally
+for
+from
+global
+if
+import
+in
+is
+lambda
+nonlocal
+not
+or
+pass
+raise
+return
+try
+while
+with
+yield
+"""
+
